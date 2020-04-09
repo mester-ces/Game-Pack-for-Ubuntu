c9922f1e4a6f3d95866be951d0dd5824a7b1ba10
mester-ces <52388120+mester-ces@users.noreply.github.com>
2020-04-09 03:13:05 +0200

    Update Gamepak for ubuntu.sh

--- Gamepak for ubuntu.sh
+++ Gamepak for ubuntu.sh
@@ -5,7 +5,7 @@ wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
 
 
- 	sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main
+ 	sudo apt-add-repository deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main
      sudo apt update
      sudo apt install --install-recommends winehq-stable
 
