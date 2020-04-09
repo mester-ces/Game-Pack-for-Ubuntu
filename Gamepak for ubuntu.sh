37aedbd944794ba1d8391afbf0576378fcdd22f1
mester-ces <52388120+mester-ces@users.noreply.github.com>
2020-04-09 15:14:15 +0200

    Add files via upload

--- Gamepak for ubuntu.sh
+++ Gamepak for ubuntu.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 sudo dpkg --add-architecture i386 
 
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
-
-
- 	sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main
-     sudo apt update
-     sudo apt install --install-recommends winehq-stable
+sudo apt install wine-stable -y
 
 
 sudo add-apt-repository ppa:lutris-team/lutris
 sudo apt-get update
-sudo apt-get install lutris
+sudo apt-get install lutris -y 
 
 
 wget https://www.playonlinux.com/script_files/PlayOnLinux/4.3.4/PlayOnLinux_4.3.4.deb
 
 sudo dpkg -i PlayOnLINUX_4.3.4.deb
 
-sudo apt install dosbox 
+sudo apt install dosbox -y
 
 sudo add-apt-repository ppa:ppsspp/stable
 sudo apt-get update
-sudo apt-get install psspp
+sudo apt-get install psspp -y 
 
-sudo apt install dsmume
+sudo apt install dsmume -y
 
 wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
 
 
 sudo dpkg -i steam.deb
 
-sudo apt install nestopia
+sudo apt install nestopia -y
